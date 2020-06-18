98ab8c5f516f752d777fe412e56d712415d78202
quannl280201 <83nguyenlapquan@gmail.com>
2020-06-18 15:10:28 +0700

    Update: Reset to 00 when reach 24

--- hour_control.v
+++ hour_control.v
@@ -7,9 +7,16 @@ module hour_control(clk, set_ena, up, down, BCD_out, TC);
 	output TC;
 	
 	wire TC_out;
-	mod_N_counter #(.MODE_N(10)) last_digit		
-		(.clk(clk), .set_ena(set_ena), .up(up), .down(down), .BCD_out(BCD_out[3:0]), .TC(TC_out));
-	mod_N_counter #(.MODE_N(2)) first_digit
-		(.clk(TC_out), .set_ena(set_ena), .up(up), .down(down), .BCD_out(BCD_out[7:4]), .TC(TC));
+	reg reset;
+	
+	always@(posedge clk) begin
+		if(BCD_out == 8'b00100010) reset = 1'b1;
+		else reset = 1'b0;
+	end
+			
+	mod_N_counter_2 #(.MODE_N(10)) last_digit		
+		(.clk(clk), .set_ena(set_ena), .reset(reset),.up(up), .down(down), .BCD_out(BCD_out[3:0]), .TC(TC_out));
+	mod_N_counter_2 #(.MODE_N(3)) first_digit
+		(.clk(TC_out), .set_ena(set_ena),.reset(reset), .up(up), .down(down), .BCD_out(BCD_out[7:4]), .TC(TC));
 	
 endmodule 
